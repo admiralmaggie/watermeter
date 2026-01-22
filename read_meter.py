de6878e6cd4a2a51e8ee809a489a3cb202d50e47
DCSLLC <arouhi@districtconsulting.com>
2026-01-22 18:01:37 -0500

    code cleanup

--- read_meter.py
+++ read_meter.py
@@ -300,7 +300,7 @@ def read_meter_from_frame(frame, motion_detector=None, show_gui=False):
         if show_gui:
             cv2.circle(output, (x, y), r, COLOR_GREEN, 3)
             cv2.line(output, (x, y), tip, COLOR_MAGENTA, 2)
-            cv2.putText(output, f"{val:.1f}", (x-20, y+r+50), cv2.FONT_HERSHEY_DUPLEX, 3, COLOR_BLUE)
+            cv2.putText(output, f"{val:.1f}", (x-20, y+r+50), cv2.FONT_HERSHEY_DUPLEX, 3, COLOR_BLUE, 2)
     
     reading = process_values(values)
     if show_gui:
